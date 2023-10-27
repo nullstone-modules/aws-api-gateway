a09a3ab966a45a1ca2420b4438869562e63700bd
Scott Sickles <scottsickles@gmail.com>
2023-10-27 15:22:47 -0400

    we will just use regional for now

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "endpoint_types" {
-  type        = list(string)
-  default     = ["EDGE"]
-  description = <<EOF
-A list of endpoint types to configure. Valid values that can be included in the list are "EDGE", "REGIONAL", and "PRIVATE".
-EOF
-}
