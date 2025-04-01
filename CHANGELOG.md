29c4e67f077891a94bf5a464c7efa74649917127
Brad Sickles <brad.sickles@gmail.com>
2025-04-01 10:42:50 -0400

    Use SSL certificate from connected subdomain if it created one.

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 0.5.8 (Unreleased)
+* Use SSL certificate from connected subdomain if it created one.
--- https.tf
+++ https.tf
@@ -7,10 +7,12 @@ resource "aws_acm_certificate" "this" {
   validation_method         = "DNS"
   subject_alternative_names = local.alternative_names
   tags                      = local.tags
+
+  count = local.create_cert ? 1 : 0
 }
 
 locals {
-  dvos = [for dvo in aws_acm_certificate.this.domain_validation_options : {
+  dvos = [for dvo in try(aws_acm_certificate.this[0].domain_validation_options, []) : {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
     type   = dvo.resource_record_type
@@ -18,7 +20,7 @@ locals {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = length(local.alternative_names) + 1
+  count = local.create_cert ? length(local.alternative_names) + 1 : 0
 
   allow_overwrite = true
   name            = local.dvos[count.index].name
@@ -29,10 +31,17 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "this" {
-  certificate_arn         = aws_acm_certificate.this.arn
+  certificate_arn         = aws_acm_certificate.this[count.index].arn
   validation_record_fqdns = [for cv in aws_route53_record.cert_validation : cv.fqdn]
 
   timeouts {
     create = "5m"
   }
+
+  count = local.create_cert ? 1 : 0
+}
+
+locals {
+  create_cert     = !local.subdomain_has_certificate
+  certificate_arn = local.subdomain_has_certificate ? local.subdomain_certificate_arn : aws_acm_certificate_validation.this[0].certificate_arn
 }
